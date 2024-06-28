c2857ab0241c4c428f79e8b41a5427680a5ac3ac
Doru991 <dorurogoveanu123@gmail.com>
2024-06-28 11:23:47 +0300

    Database connection

---
+++ SmartWorkout.DBAccess/Repository/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using SmartWorkout.DBAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWorkout.DBAccess.Repository
+{
+    public interface IUserRepository : IDisposable
+    {
+        public Task<User[]> GetUsers();
+        public Task<User?> GetUserById(int id);
+        public void AddUser(User user);
+        public void DeleteUser(int userID);
+        public void UpdateUser(User user);
+        public void Save();
+    }
+}
---
+++ SmartWorkout.DBAccess/Repository/UserRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartWorkout.DBAccess.Entities;
+namespace SmartWorkout.DBAccess.Repository
+{
+    public class UserRepository : IUserRepository, IDisposable
+    {
+        private SmartWorkoutContext context;
+        public UserRepository(SmartWorkoutContext context)
+        {
+            this.context = context; 
+        }
+        public async Task<User[]> GetUsers()
+        {
+            return await context.Users.ToArrayAsync<User>();
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+        public void DeleteUser(int userID)
+        {
+            User? user = context.Users.Find(userID);
+            if(user!=null)
+            {
+                context.Users.Remove(user);
+            }
+        }
+        public void UpdateUser(User user)
+        {
+            context.Entry(user).State=EntityState.Modified;
+        }
+        public void AddUser(User user)
+        {
+            context.Users.Add(user);
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
