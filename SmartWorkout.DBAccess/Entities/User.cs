67789da112447bbad3ef021acbf7c078a5edafef
Doru991 <dorurogoveanu123@gmail.com>
2024-07-03 02:15:55 +0300

    Form validation

--- SmartWorkout.DBAccess/Entities/User.cs
+++ SmartWorkout.DBAccess/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,20 @@ namespace SmartWorkout.DBAccess.Entities
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", ErrorMessage = "The name is written incorrectly")]
         public string Name { get; set; } = null!;
+        [Required]
+        [RegularExpression(@"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", ErrorMessage = "The surname is written incorrectly")]
         public string Surname { get; set; } = null!;
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Email is not correctly formatted")]
         public string? Email { get; set; }
+        [StringLength(15)]
+        [RegularExpression(@"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", ErrorMessage = "Unexpected characters in phone number")]
         public string? Phone { get; set; }
+        [Range(25, 400, ErrorMessage = "Weight must be between 25 and 400 kg")]
         public double? Weight { get; set; }
+        [Range(10, 100, ErrorMessage = "Age must be between 10 and 100")]
         public int? Age { get; set; }
 
         public ICollection<Workout> Workouts { get; set; } = new HashSet<Workout>();
