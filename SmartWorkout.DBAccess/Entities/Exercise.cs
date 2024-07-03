ee720d8ed6731fb72bd06452a1707c85b418a31f
Doru991 <dorurogoveanu123@gmail.com>
2024-07-03 09:52:37 +0300

    Exercise CRUD

--- SmartWorkout.DBAccess/Entities/Exercise.cs
+++ SmartWorkout.DBAccess/Entities/Exercise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace SmartWorkout.DBAccess.Entities
     public class Exercise
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
         public ICollection<ExerciseLog> Logs { get; set; } = null!;
     }
